3c4f9d0969be97b2937f9e02752187677a7c0e5e
Yehor Ushakov <trpdjkework@gmail.com>
2023-12-25 00:55:45 +0200

    re frst s

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -1,10 +1,9 @@
 from aiogram import Bot
 from aiogram.types import Message
 
-api = 'https://docs.stratz.com/swagger/v1/swagger.json'
-
+from core.keyboards.skins import main_kb
 
 async def start_com(message: Message, bot: Bot):
-    await message.answer('Уебище я твою мать!')
+    await message.answer("Привіт це для покупок есклюзивних товарів по грі дота 2, можеш ознаоймитися з моїми функціями нижче", reply_markup=main_kb)
 
 
--- core/handlers/hero.py
+++ core/handlers/hero.py
@@ -1,11 +1,11 @@
-from aiogram import Bot
-from aiogram.types import Message
+from aiogram import Bot, types
+from aiogram.types import Message, FSInputFile, InputFile,  InlineKeyboardButton, InlineKeyboardMarkup
 from core.middlewareas.libary import dota2_heroes_dict
 import json
 
-
 with open('lib.json', 'r') as file:
     data = json.load(file)
+
 
 def find_key_by_value(dictionary, search_value):
     for key, values in dictionary.items():
@@ -19,10 +19,4 @@ async def send_hero_link(message: Message, bot: Bot):
     if hero_name == '404':
         await message.answer('Герой не найден')
     else:
-        for set_name, set_info in data[hero_name].items():
-            print(f"Set Name: {set_name}")
-            print(f"Image: {set_info['img']}")
-            print(f"Price: {set_info['price']}")
-            print(f"Availability: {'Available' if set_info['availability'] else 'Not Available'}")
-            print(f"Type: {set_info['type']}")
-            print("-" * 30)
+        await message.answer('Герой найден')
